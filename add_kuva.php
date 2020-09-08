5dc1bb19aec1fcebed0431c30f8a7f305e4307f1
Tuomas <tuomas.puro@esedu.fi>
2020-09-08 10:27:28 +0300

    Pientä järjestelyä

---
+++ add_kuva.php
@@ -0,0 +1,39 @@
+<?php  
+session_start();
+
+
+if (!isset(($_SESSION['username'])){
+    echo "ei lupaa tälle sivulle";
+    die();
+}
+//$kuva = imagecreatetruecolor(120, 20);
+
+//$text_color = imagecolorallocate($kuva , 233, 14, 91); 
+
+//imagestring($kuva, 1, 5, 5, 'string', $text_color);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<img src="paraiso.jpg" alt="">
+</div>
+<div>
+<button id="btn"></button>
+</div>
+
+<?php 
+ $kuva = $_GET["kuva"];
+ 
+ 
+
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+session_start() 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="galleria.xml">
+<link rel="stylesheet" href="add_kuva.php">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<h1>kuvat</h1>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kuvat</title>
+</head>
+<body>
+<div class="container">
+  <li><a href="register.php">kirjaudu</a></li>
+  <li><a href="add_kuva.php">lisää kuva</a></li>
+</div>
+<div>
+<img src="img/paraiso.jpg" alt="paratiisi">
+
+</div>
+<script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  //<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  //<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  //<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+</script>
+</body>
+</html>
